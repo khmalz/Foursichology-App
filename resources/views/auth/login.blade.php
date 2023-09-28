b56ac8cfe80e7280d60685dab043da12cb6f57f9
khmalz <akmalrma1710@gmail.com>
2023-09-28 09:50:58 +0700

    optimize app by deleting unused file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,6 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ asset('frontend/plugins/fontawesome-free/css/all.min.css') }}">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
